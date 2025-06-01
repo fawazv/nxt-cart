024b3820fed3a760125be62e75cdb65870eebf81
Fawaz v <fawazchemmad@gmail.com>
2025-06-01 10:21:41 +0530

    create browsing history

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,6 +9,7 @@ import data from '@/lib/data'
 import { toSlug } from '@/lib/utils'
 import ProductSlider from '@/components/shared/product/product-slider'
 import { Card, CardContent } from '@/components/ui/card'
+import BrowsingHistoryList from '@/components/shared/browsing-history-list'
 
 export default async function HomePage() {
   const categories = (await getAllCategories()).slice(0, 4)
@@ -87,6 +88,9 @@ export default async function HomePage() {
             />
           </CardContent>
         </Card>
+      </div>
+      <div className="p-4 bg-background">
+        <BrowsingHistoryList />
       </div>
     </>
   )
